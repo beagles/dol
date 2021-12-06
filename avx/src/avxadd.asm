e7c2013a20ef11da8bc9bc3a51c4a7d470cf3b91
Brent Eagles <brent.eagles@gmail.com>
2021-12-06 13:09:19 -0330

    A bit of cleanup and add div operation

--- avx/src/avxadd.asm
+++ avx/src/avxadd.asm
@@ -1,14 +1,42 @@
-; Simple addition of two arrays of integers 
+; Simple addition of two arrays of integers
 section .text
-global _avxadd
+global _avxadd, _avxdiv
+
 _avxadd:
+        push            rbp                     ; I don't think these are
+                                                ; necessary. The callee is
+                                                ; supposedly responsible for this
+        mov             rbp, rsp
+
+        vzeroall                                ; zero all the registers
+                                                ; this probably is redundant
+                                                ; it might help catch errors
+                                                ; while debugging, but I suspect
+                                                ; in this case it's a waste of
+                                                ; cycles
+
+        vmovups         ymm0, [rdi]             ; move 1st argument to ymm0
+        vmovups         ymm1, [rsi]             ; move 2nd argument to ymm1
+
+        vaddps          ymm2, ymm0, ymm1        ; add 1st and 2nd arguments,
+                                                ; putting result in ymm2
+
+        vmovups         [rdx], ymm2             ; store the result in array that
+                                                ; is the 3rd argument
+
+        pop     rbp
+        ret
+
+
+_avxdiv:
         push            rbp
         mov             rbp, rsp
         vzeroall
         vmovups         ymm0, [rdi]
         vmovups         ymm1, [rsi]
-        vaddps          ymm2, ymm0, ymm1
+        vdivps          ymm2, ymm0, ymm1
         vmovups         [rdx], ymm2
 
         pop     rbp
         ret
+
--- avx/src/avxaddmain.cpp
+++ avx/src/avxaddmain.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 
 extern "C" void avxadd(float a[], float b[], float result[]);
+extern "C" void avxdiv(float a[], float factor[], float result[]);
 
 typedef float __attribute__((__aligned__(64))) al_float;
 
 al_float v1 [] = { 10.0, 20.0, 30.0, 40.0, 50.0, 60.0 };;
-al_float v2 [] = { 100.0, 200.0, 300.0, 400.0, 500.0, 600.0 };;
+al_float v2 [] = { 100.0, 200.0, 300.0, 400.0, 500.0, 600.0 };
+float divisor[] = { 0.3, -1.4, 2.5, 0.666666, 50.2, 100.2 };
 al_float vresult [6];
+al_float divresult[6];
 
 int main(int, char**)
 {
     avxadd(v1, v2, vresult);
+    avxdiv(vresult, divisor, divresult);
     for( int i = 0; i < 6; ++i)
     {
         std::cout << v1[i] << " + " << v2[i] << " = " << vresult[i] << std::endl;
+        std::cout << vresult[i] << " / " << divisor[i]  << " = " << divresult[i] << std::endl;
     }
     return 0;
 }
